fdaccc2ee80ee882be25fe95d588cf4fb37328ba
董家成 <ddavid0706tw@gmail.com>
2019-01-04 09:26:15 +0800

    Java and Kotlin Ticket homework

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -7,7 +7,6 @@ public class Tester {
     public static void main(String[] args) {
         Ticket ticket = new Ticket();
         Scanner scanner = new Scanner(System.in);
-        //int choose = scanner.nextInt();
         System.out.print("Please enter number of tickets:");
         int totalmany = scanner.nextInt();
         System.out.print("How many round-trip tickets:");
